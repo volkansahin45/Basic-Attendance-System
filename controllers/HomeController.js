cc7d8c21e70bc6c97eba0384f0c64c299df69d63
Volkan Sahin <beyvolkantr@gmail.com>
2017-03-04 00:00:40 +0300

    Edit worktime function

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -46,12 +46,13 @@ exports.lastStatus = function (req, res) {
 //get users working times
 exports.worktime = function (req, res) {
   console.log("Work Time girildi");
-  var time = req.body.time;
-  console.log(time);
-  db.query("SELECT fk_user as userid, SUM(UNIX_TIMESTAMP(time) - UNIX_TIMESTAMP(GIRIS_ZAMANI)) AS fark FROM" +
-    "(SELECT CIK.*,(SELECT MAX(GIR.time) FROM psh_raw GIR WHERE GIR.fk_user = CIK.fk_user AND GIR.time <= CIK.time and GIR.direct = 1) AS GIRIS_ZAMANI" +
-    " FROM `psh_raw` CIK WHERE CIK.direct = 0 AND date_format( time, '%Y-%m' ) = ?" +
-    ") AS V1 GROUP BY fk_user", [time], function (err, result) {
+  var startDate = req.body.startDate;
+  var finishDate = req.body.finishDate;
+
+  console.log("StartDate : " + startDate + "-FinishDate : " + finishDate);
+  var query = "SELECT V2.fk_user,U.name, V2.Date, SUM(V2.Fark) AS 'workingtime' FROM (SELECT V1.fk_user, date_format( V1.Cikis_Zamani, '%Y-%m-%d') AS 'date', V1.Fark FROM (SELECT CIK.rid, CIK.fk_user ,(SELECT MAX(GIR.time) FROM psh_raw GIR WHERE GIR.fk_user = CIK.fk_user AND GIR.time <= CIK.time AND GIR.direct = 1) AS 'Giris_Zamani', CIK.time AS 'Cikis_Zamani', (SELECT UNIX_TIMESTAMP(Cikis_Zamani) - UNIX_TIMESTAMP(Giris_Zamani)) AS Fark FROM `psh_raw` CIK WHERE CIK.direct = 0) AS V1) AS V2 INNER JOIN user U ON V2.fk_user = U.uid GROUP BY V2.fk_user, V2.Date HAVING V2.Date BETWEEN ? AND ?";
+
+  db.query(query, [startDate, finishDate], function (err, result) {
       if (err) {
         return util.send(res, 400, "Database Error");
       }
