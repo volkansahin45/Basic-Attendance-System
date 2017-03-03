19d90555f07f9dcf4a73362ca9f356ca317c4d33
Volkan Sahin <beyvolkantr@gmail.com>
2017-03-04 01:29:13 +0300

    Upcase letter problem fixed

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -50,7 +50,7 @@ exports.worktime = function (req, res) {
   var finishDate = req.body.finishDate;
 
   console.log("StartDate : " + startDate + "-FinishDate : " + finishDate);
-  var query = "SELECT V2.fk_user,U.name, V2.Date, SUM(V2.Fark) AS 'workingtime' FROM (SELECT V1.fk_user, date_format( V1.Cikis_Zamani, '%Y-%m-%d') AS 'date', V1.Fark FROM (SELECT CIK.rid, CIK.fk_user ,(SELECT MAX(GIR.time) FROM psh_raw GIR WHERE GIR.fk_user = CIK.fk_user AND GIR.time <= CIK.time AND GIR.direct = 1) AS 'Giris_Zamani', CIK.time AS 'Cikis_Zamani', (SELECT UNIX_TIMESTAMP(Cikis_Zamani) - UNIX_TIMESTAMP(Giris_Zamani)) AS Fark FROM `psh_raw` CIK WHERE CIK.direct = 0) AS V1) AS V2 INNER JOIN user U ON V2.fk_user = U.uid GROUP BY V2.fk_user, V2.Date HAVING V2.Date BETWEEN ? AND ? ORDER BY fk_user";
+  var query = "SELECT V2.fk_user,U.name, V2.date, SUM(V2.Fark) AS 'workingtime' FROM (SELECT V1.fk_user, date_format( V1.Cikis_Zamani, '%Y-%m-%d') AS 'date', V1.Fark FROM (SELECT CIK.rid, CIK.fk_user ,(SELECT MAX(GIR.time) FROM psh_raw GIR WHERE GIR.fk_user = CIK.fk_user AND GIR.time <= CIK.time AND GIR.direct = 1) AS 'Giris_Zamani', CIK.time AS 'Cikis_Zamani', (SELECT UNIX_TIMESTAMP(Cikis_Zamani) - UNIX_TIMESTAMP(Giris_Zamani)) AS Fark FROM `psh_raw` CIK WHERE CIK.direct = 0) AS V1) AS V2 INNER JOIN user U ON V2.fk_user = U.uid GROUP BY V2.fk_user, V2.Date HAVING V2.Date BETWEEN ? AND ? ORDER BY fk_user";
 
   db.query(query, [startDate, finishDate], function (err, result) {
       if (err) {
