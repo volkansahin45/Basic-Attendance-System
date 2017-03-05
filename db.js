22cecb94bc1afa5d1ed17cf83b7109472b1fae2b
Volkan Sahin <beyvolkantr@gmail.com>
2017-03-05 11:01:55 +0300

    Fixed 'PROTOCOL_CONNECTION_LOST' database problem

--- db.js
+++ db.js
@@ -1,9 +1,31 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var db_config = {
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'attandence'
-});
+}
+var connection;
 
-module.exports=connection;
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+module.exports = connection;
